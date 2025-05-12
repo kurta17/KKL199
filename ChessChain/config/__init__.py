68e87e084a7dea5f46d24064f949fde1f4121fb9
Giorgi Kurtanidze <thinkers.struggle@gmail.com>
2025-05-12 18:04:58 +0700

    Add IPv8 configuration module

---
+++ ChessChain/config/__init__.py
@@ -0,0 +1,8 @@
+"""ChessChain configuration module.
+
+This module contains configuration utilities for IPv8 and the chess community.
+"""
+
+from config.config import create_ipv8_config
+
+__all__ = ['create_ipv8_config']
