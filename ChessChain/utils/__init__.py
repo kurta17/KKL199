339cbf12674e7e97a2b628640bcbc59a5d8f6ff1
Giorgi Kurtanidze <thinkers.struggle@gmail.com>
2025-05-14 00:50:19 +0700

    Import lottery_selection solved

--- ChessChain/utils/__init__.py
+++ ChessChain/utils/__init__.py
@@ -3,7 +3,7 @@
 This module contains helper functions and interface utilities.
 """
 
-from utils.utils import check_port, vrf_sortition
+from utils.utils import check_port, lottery_selection
 from utils.interface import manual_send_loop, print_help
 
-__all__ = ['check_port', 'vrf_sortition', 'manual_send_loop', 'print_help']
+__all__ = ['check_port', 'lottery_selection', 'manual_send_loop', 'print_help']
